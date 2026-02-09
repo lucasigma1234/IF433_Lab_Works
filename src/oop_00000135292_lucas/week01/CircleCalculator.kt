9ad29a0561a6f1bdb3138b08b038c1ce10f12759
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-02-09 19:32:23 +0700

    week01: (tugas 1) clean up unused args and comments

--- src/oop_00000135292_lucas/week01/CircleCalculator.kt
+++ src/oop_00000135292_lucas/week01/CircleCalculator.kt
@@ -1,10 +1,8 @@
 package oop_00000135292_lucas.week01
 
-fun main(args: Array<String>) {
-
+fun main() {
     val radius = 7.0
     val pi = 3.14
-
     val area = pi * radius * radius
 
     println("Radius: $radius, Area: $area")
