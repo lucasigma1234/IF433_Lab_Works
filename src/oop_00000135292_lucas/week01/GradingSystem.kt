082b7d1bdaac4fcbc19f2e68e5eda70728d47ff8
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-02-09 18:59:13 +0700

    week01: refactor to use val and string templates

--- src/oop_00000135292_lucas/week01/GradingSystem.kt
+++ src/oop_00000135292_lucas/week01/GradingSystem.kt
@@ -3,5 +3,5 @@ package oop_00000135292_lucas.week01
 fun main() {
     var name: string = "lucas"
     var score: int = 80
-    println("Nama: " + name + ", Nilai: "+ score)
+    println("Nama: $name + , Nilai: $score")
 }
