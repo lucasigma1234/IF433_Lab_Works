7db09b2294ac6ebfc852fb9b989ae89d694db526
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-02-09 19:38:23 +0700

    week01: (tugas 2) clean up main function and use expression body

--- src/oop_00000135292_lucas/week01/GameStore.kt
+++ src/oop_00000135292_lucas/week01/GameStore.kt
@@ -1,7 +1,6 @@
 package oop_00000135292_lucas.week01
 
-fun main(args: Array<String>) {
-
+fun main() {
     val gameName = "Elden Ring"
     val price = 750000
     val isDiscount = true
@@ -12,6 +11,5 @@ fun main(args: Array<String>) {
     println("Final Price: $finalPrice")
 }
 
-fun calculateFinalPrice(price: Int, isDiscount: Boolean): Int {
-    return if (isDiscount) price - 200000 else price
-}
+fun calculateFinalPrice(price: Int, isDiscount: Boolean) =
+    if (isDiscount) price - 200000 else price
