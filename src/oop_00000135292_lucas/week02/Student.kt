bc28e73b4351986a64d8eca8ac244a73b10f10c3
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-02-11 15:44:05 +0700

    week02: add warning validation in init block

--- src/oop_00000135292_lucas/week02/Student.kt
+++ src/oop_00000135292_lucas/week02/Student.kt
@@ -1,9 +1,16 @@
 package oop_00000135292_lucas.week02
 
-class Student (
-    val name: String,
-    val nim: String,
-    var major: String,
-){
+class Student (val name: String, val nim: String, val major: String) {
+    init {
+        if (nim.length != 5) {
+            println("Warning: objek tercipta dengan NIL ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah disistem.")
+        } else {
+            println("Log: objek student $name berhasil dialokasikan di memory.")
+        }
+    }
+}
+
+{
     //body class
 }
