c8b7ebd3b924f5c090a3e08418010957d226fffb
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-02-18 11:11:15 +0700

    week02: (task 1) implement library input system

--- src/oop_00000135292_lucas/week02/Loan.kt
+++ src/oop_00000135292_lucas/week02/Loan.kt
@@ -1,16 +1,32 @@
 package oop_00000135292_lucas.week02
 
-class Loan(
-    val bookTitle: String,
-    val borrower: String,
-    val loanDuration: Int = 1
-) {
+import java.util.Scanner
 
-    fun calculateFine(): Int {
-        return if (loanDuration > 3) {
-            (loanDuration - 3) * 2000
-        } else {
-            0
-        }
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- SISTEM PEMINJAMAN BUKU ---")
+
+    print("Judul Buku: ")
+    val title = scanner.nextLine()
+
+    print("Nama Peminjam: ")
+    val borrower = scanner.nextLine()
+
+    print("Lama Pinjam (hari): ")
+    var duration = scanner.nextInt()
+
+    // Validasi sesuai modul
+    if (duration < 0) {
+        println("Durasi tidak valid, otomatis diset ke 1 hari.")
+        duration = 1
     }
+
+    val loan = Loan(title, borrower, duration)
+
+    println("\n--- DETAIL PEMINJAMAN ---")
+    println("Judul Buku : ${loan.bookTitle}")
+    println("Peminjam  : ${loan.borrower}")
+    println("Durasi    : ${loan.loanDuration} hari")
+    println("Denda     : Rp ${loan.calculateFine()}")
 }
