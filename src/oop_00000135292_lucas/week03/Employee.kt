9fdfe846fe5083d5bbaf771f296230b9d7f0c497
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-02-24 21:15:51 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000135292_lucas/week03/Employee.kt
+++ src/oop_00000135292_lucas/week03/Employee.kt
@@ -1,5 +1,10 @@
 package oop_00000135292_lucas.week03
 
-class Employee(val nama: String) {
+class Employee(
+    val name: String
+) {
     var salary: Int = 0
+        set(value) {
+            this.salary = value // ❌ SALAH → StackOverflow
+        }
 }
