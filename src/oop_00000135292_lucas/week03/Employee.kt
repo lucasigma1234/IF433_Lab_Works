a2dafa444d851d4207240fa5edce8344f8b81e81
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-02-24 21:31:20 +0700

    week03: fix setter using backing field

--- src/oop_00000135292_lucas/week03/Employee.kt
+++ src/oop_00000135292_lucas/week03/Employee.kt
@@ -1,20 +1,11 @@
 package oop_00000135292_lucas.week03
 
-class Employee(
-    val name: String
-) {
+class Employee(val name: String) {
+
     var salary: Int = 0
         set(value) {
             if (value >= 0) {
                 field = value
             }
         }
-
-    private var performanceRating: Int = 0
-
-    fun updatePerformance(rating: Int) {
-        if (rating in 1..5) {
-            performanceRating = rating
-        }
-    }
 }
