27af8d3db28de14f87b3b9f65d891dbaa7df3892
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-03-04 20:12:35 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000135292_lucas/week05/MathHelper.kt
@@ -0,0 +1,13 @@
+package oop_00000135292_lucas.week05
+
+class MathHelper {
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
