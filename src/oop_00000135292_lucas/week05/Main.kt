0f0017f814abb251f4d3420fba838ca82f43916a
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-03-04 20:15:17 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_00000135292_lucas/week05/Main.kt
+++ src/oop_00000135292_lucas/week05/Main.kt
@@ -22,4 +22,12 @@ fun main() {
         }
         println("------------------------------")
     }
+    println("\n=== TESTING MATH HELPER (OVERLOADING) ===")
+    val math = MathHelper()
+    val luasPersegi = math.hitungLuas(5)
+    println("Luas Persegi (sisi 5)          : $luasPersegi")
+    val luasPersegiPanjang = math.hitungLuas(10, 5)
+    println("Luas Persegi Panjang (10x5)    : $luasPersegiPanjang")
+    val luasLingkaran = math.hitungLuas(7.0)
+    println("Luas Lingkaran (r 7.0)         : $luasLingkaran")
 }
