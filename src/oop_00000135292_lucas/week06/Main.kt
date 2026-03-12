98845b1048116e56c8acf19c82dff9bfd4a2ecc8
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-03-12 12:08:19 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/oop_00000135292_lucas/week06/Main.kt
+++ src/oop_00000135292_lucas/week06/Main.kt
@@ -4,17 +4,34 @@ fun processCheckout(method: PaymentMethod, amount: Double) {
     println("-> Memulai checkout...")
     method.pay(amount)
 }
+
 fun main() {
-    val myWatch= showTime()
+
+    val myWatch = Smartwatch()
     myWatch.showTime()
 
-    val myPhone= Smartphone()
+    val myPhone = Smartphone()
     myPhone.turnOn()
 
-    val pay1= Gopay()
-    val pay2= CreditCard()
+    val pay1 = Gopay()
+    val pay2 = CreditCard()
 
     println("\n=== TESTING CHECKOUT ===")
+
     processCheckout(method = pay1, amount = 50000.0)
     processCheckout(method = pay2, amount = 150000.0)
+
+    println("\n=== SMART HOME TEST ===")
+
+    val hub = SmartHomeHub()
+
+    val lamp = SmartLamp("01", "Lampu Ruang Tamu")
+    val speaker = SmartSpeaker("02", "Google Nest")
+    val cctv = SmartCCTV("03", "CCTV Depan Rumah")
+
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    hub.activateSecurityMode()
 }
