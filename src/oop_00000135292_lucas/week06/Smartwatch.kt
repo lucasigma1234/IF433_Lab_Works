556bf81ebf262dc2ff57267d02f3c233d642b355
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-03-12 10:38:41 +0700

    week06: create Smartwatch with multiple implementations

---
+++ src/oop_00000135292_lucas/week06/Smartwatch.kt
@@ -0,0 +1,13 @@
+package oop_00000135292_lucas.week06
+
+class Smartwatch: Watch(), BluetoothConnectable, Rechargeable {
+    override fun showTime() {
+        println("Layar OLED menyala: 14:00 WIB")
+    }
+    override fun connectToBluetooth() {
+        println("mencari perangkat hp di sekitar untuk pairing...")
+    }
+    override fun chargeBaterry(){
+        println("mengisi daya menggunakan charger magnetik 15W.")
+    }
+}
