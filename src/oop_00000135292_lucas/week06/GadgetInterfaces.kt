d17285e75a3dec4963bc0b223255ad4d7afbd378
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-03-12 10:42:29 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000135292_lucas/week06/GadgetInterfaces.kt
@@ -0,0 +1,12 @@
+package oop_00000135292_lucas.week06
+
+interface camera {
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+interface Phone {
+    fun turnOn( {
+        println("Sinyal seluler mencari jaringan")
+    })
+}
