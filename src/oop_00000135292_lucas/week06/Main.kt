63ba42d92efb5de684a98e1e10270c6c7f09d70a
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-03-12 12:11:59 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_00000135292_lucas/week06/Main.kt
+++ src/oop_00000135292_lucas/week06/Main.kt
@@ -7,31 +7,32 @@ fun processCheckout(method: PaymentMethod, amount: Double) {
 
 fun main() {
 
+    // === TESTING WATCH ===
     val myWatch = Smartwatch()
     myWatch.showTime()
 
+    // === TESTING PHONE ===
     val myPhone = Smartphone()
     myPhone.turnOn()
 
+    // === TESTING PAYMENT ===
     val pay1 = Gopay()
     val pay2 = CreditCard()
 
     println("\n=== TESTING CHECKOUT ===")
-
     processCheckout(method = pay1, amount = 50000.0)
     processCheckout(method = pay2, amount = 150000.0)
 
+    // === SMART HOME TEST ===
     println("\n=== SMART HOME TEST ===")
 
     val hub = SmartHomeHub()
 
-    val lamp = SmartLamp("01", "Lampu Ruang Tamu")
-    val speaker = SmartSpeaker("02", "Google Nest")
-    val cctv = SmartCCTV("03", "CCTV Depan Rumah")
+    val lamp = SmartLamp("01", "Ruang Tamu")
+    val speaker = SmartSpeaker("02", "Google Nest Dapur")
+    val cctv = SmartCCTV("03", "Ezviz Garasi")
 
     hub.addDevice(lamp)
     hub.addDevice(speaker)
     hub.addDevice(cctv)
-
-    hub.activateSecurityMode()
 }
