a053b61ddb7c30e0279474499379b35d59b0e1fc
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-03-25 16:22:49 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000135292_lucas/week07/GameModels.kt
@@ -0,0 +1,7 @@
+package oop_00000135292_lucas.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70), UNCOMMON(25), RARE(10), EPIC(4), LEGENDARY(1)
+}
+
+data class GameItem(val name: String, val damage: Int, val rarity: ItemRarity)
