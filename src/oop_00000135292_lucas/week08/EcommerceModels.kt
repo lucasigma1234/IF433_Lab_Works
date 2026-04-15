b451a1f6a1ced2adef82abba2c7ddfbff955312c
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-04-15 21:19:55 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_00000135292_lucas/week08/EcommerceModels.kt
@@ -0,0 +1,13 @@
+package oop_00000135292_lucas.week08
+
+sealed class Product
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()
---
+++ src/oop_00000135292_lucas/week08/JavaPaymentService.java
@@ -0,0 +1,4 @@
+package oop_00000135292_lucas.week08;
+
+public class JavaPaymentService {
+}
