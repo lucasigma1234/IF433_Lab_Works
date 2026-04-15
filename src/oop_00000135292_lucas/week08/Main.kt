4219797d4b5a94c4e1755f6a4096f84c73dcd3aa
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-04-15 20:44:57 +0700

    week08: implement java interop using !!

--- src/oop_00000135292_lucas/week08/Main.kt
+++ src/oop_00000135292_lucas/week08/Main.kt
@@ -46,4 +46,8 @@ fun main() {
     } catch (e: IllegalArgumentException) {
         println(e.message)
     }
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+    val statusLength = javaResponse!!.length
+    println("status dari jawa: $javaResponse(Length: $statusLength) ")
 }
