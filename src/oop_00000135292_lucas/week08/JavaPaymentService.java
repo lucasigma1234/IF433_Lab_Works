0e1ffde7a63d2cdc5040acb218d0c3819159ea57
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-04-15 21:21:59 +0700

    week08: (task) create JavaPaymentService for payment interop

--- src/oop_00000135292_lucas/week08/JavaPaymentService.java
+++ src/oop_00000135292_lucas/week08/JavaPaymentService.java
@@ -1,4 +1,7 @@
 package oop_00000135292_lucas.week08;
 
 public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
 }
