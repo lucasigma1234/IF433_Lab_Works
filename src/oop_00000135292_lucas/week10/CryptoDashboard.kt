f2a31498078e183ace87ff1889c4f1b0068d4349
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-04-29 13:29:38 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000135292_lucas/week10/CryptoDashboard.kt
+++ src/oop_00000135292_lucas/week10/CryptoDashboard.kt
@@ -5,4 +5,5 @@ fun main() {
     coinRepo.add(Coin("BTC", 0.5))
     coinRepo.add(Coin("ETH", 10.2))
     coinRepo.add(Coin("USDT", 1500.0))
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
