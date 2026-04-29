d3106379a3808b472ea0ed7760fc8760a509666e
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-04-29 13:19:29 +0700

    week10: (task) create Coin and Transaction models

---
+++ src/oop_00000135292_lucas/week10/WalletModels.kt
@@ -0,0 +1,5 @@
+package oop_00000135292_lucas.week10
+
+// Model data untuk Crypto Wallet [cite: 292, 294, 295]
+data class Coin(val name: String, val balance: Double)
+data class Transaction(val id: String, val amount: Double)
