8e760fffdacc9d590108c78b4cb84173c326406e
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-04-29 12:40:28 +0700

    week09: implement sortedByDescending and forEach

--- src/oop_00000135292_lucas/week09/HigherOrderFunctions.kt
+++ src/oop_00000135292_lucas/week09/HigherOrderFunctions.kt
@@ -7,3 +7,15 @@ fun main() {
     // filter() membuat list baru berisi elemen yang memenuhi kondisi (true) [cite: 78]
     val evens = numbers.filter { it % 2 == 0 }
     println("Evens only: $evens")
+    println("\n=== HOF: MAP ===")
+    // map() merubah bentuk/nilai data [cite: 85]
+    val multiplied = evens.map { it * 10 }
+    val asStrings = multiplied.map { "Rp ${it}k" }
+    println("Multiplied: $multiplied")
+    println("Formatted: $asStrings")
+
+    // Menambah SortedByDescending dan forEach sesuai pesan commit Checkpoint 7
+    println("\n=== HOF: SORT & FOREACH ===")
+    val sortedData = numbers.sortedByDescending { it }
+    sortedData.forEach { print("$it ") }
+}
