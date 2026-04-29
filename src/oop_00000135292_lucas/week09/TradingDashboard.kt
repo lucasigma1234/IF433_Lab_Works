6beba33425f9b3dc7bf2ab2576983023cfbf7860
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-04-29 12:49:31 +0700

    week09: (task) extract losing trades using filter

--- src/oop_00000135292_lucas/week09/TradingDashboard.kt
+++ src/oop_00000135292_lucas/week09/TradingDashboard.kt
@@ -11,3 +11,4 @@ fun main() {
     )
     val closedTrades = tradeHistory.filter { it.status == "CLOSED" }
     val winningTrades = closedTrades.filter { it.roe > 0 }
+    val losingTrades = closedTrades.filter { it.roe <= 0 }
