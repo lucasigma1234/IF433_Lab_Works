743bd7dc5ae4b17d3d54b86d59f7d5077be74dc1
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-04-29 13:17:06 +0700

    week10: implement PairBox in main function

--- src/oop_00000135292_lucas/week10/Main.kt
+++ src/oop_00000135292_lucas/week10/Main.kt
@@ -1,27 +1,13 @@
 package oop_00000135292_lucas.week10
 
 fun main() {
-    // Checkpoint 2: Test Generic Class [cite: 220, 226]
     println("=== TEST GENERIC CLASS ===")
     val intBox = Box(100)
     val stringBox = Box("Generics in Kotlin")
     println("Isi intBox: ${intBox.value}")
     println("Isi stringBox: ${stringBox.value}")
 
-    // Checkpoint 4: Test Multiple Parameters [cite: 235, 240]
     println("\n=== TEST MULTIPLE PARAMETERS ===")
     val itemPrice = PairBox("Bitcoin", 65000)
     println("Aset: ${itemPrice.key}, Harga: ${itemPrice.value} USD")
-
-    // Checkpoint 7: Test Generic Functions [cite: 258, 262]
-    println("\n=== TEST GENERIC FUNCTIONS ===")
-    printData(3.14)
-    val result = processData("Stable Coin")
-    println("Hasil proses: $result")
-
-    // Checkpoint 10: Test Constraints [cite: 283, 288]
-    println("\n=== TEST CONSTRAINTS ===")
-    val math = MathBox(10.5, 20)
-    println("Total: ${math.sum()}")
-    println("Terbesar: ${getMax(45, 90)}")
 }
