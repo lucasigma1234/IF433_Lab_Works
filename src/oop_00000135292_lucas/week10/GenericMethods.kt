b48650880b500393a6c688baf528a22246c0bea8
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-04-29 13:02:36 +0700

    week10: implement basic generic function printData

---
+++ src/oop_00000135292_lucas/week10/GenericMethods.kt
@@ -0,0 +1,6 @@
+package oop_00000135292_lucas.week10
+
+// Fungsi generik dengan awalan <T> sebelum nama fungsi [cite: 245, 247]
+fun <T> printData(data: T) {
+    println("Data yang diterima: $data")
+}
