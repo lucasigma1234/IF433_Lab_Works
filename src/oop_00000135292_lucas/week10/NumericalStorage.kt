d34d9d91d4305d2cb9172fe440785aedfac2e5a8
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-04-29 13:06:37 +0700

    week10: implement where clause for comparable constraints

--- src/oop_00000135292_lucas/week10/NumericalStorage.kt
+++ src/oop_00000135292_lucas/week10/NumericalStorage.kt
@@ -5,4 +5,8 @@ class MathBox<T: Number>(val value1: T, val value2: T) {
     fun sum(): Double {
         return value1.toDouble() + value2.toDouble() [cite: 269]
     }
+    // Menggunakan where clause agar tipe T harus bisa dibandingkan (Comparable) [cite: 276, 277]
+    fun <T> getMax(a: T, b: T): T where T: Comparable<T> {
+        return if (a > b) a else b [cite: 278]
+    }
 }
