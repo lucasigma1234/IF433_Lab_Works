7dbeed3016557426adc4c5b38fa3f763f85ca00e
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-04-29 13:27:46 +0700

    week10: (task) implement search logic in repository

--- src/oop_00000135292_lucas/week10/WalletRepository.kt
+++ src/oop_00000135292_lucas/week10/WalletRepository.kt
@@ -1,4 +1,19 @@
 package oop_00000135292_lucas.week10
 
-class WalletRepository {
+// Repository generik untuk menyimpan berbagai jenis entitas [cite: 298, 300]
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item) [cite: 301]
+    }
+
+    fun getAll(): List<T> {
+        return items [cite: 301]
+    }
+
+    // Checkpoint 14: week10: (task) implement search logic in repository [cite: 312]
+    fun find(predicate: (T) -> Boolean): T? {
+        return items.find(predicate)
+    }
 }
