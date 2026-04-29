da736f0138b9cc950b3e9fd2d9065af65fe03593
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-04-29 13:03:35 +0700

    week10: implement generic function with return type

--- src/oop_00000135292_lucas/week10/GenericMethods.kt
+++ src/oop_00000135292_lucas/week10/GenericMethods.kt
@@ -3,4 +3,8 @@ package oop_00000135292_lucas.week10
 // Fungsi generik dengan awalan <T> sebelum nama fungsi [cite: 245, 247]
 fun <T> printData(data: T) {
     println("Data yang diterima: $data")
+    // Fungsi yang mengembalikan tipe data yang sama dengan parameternya [cite: 253, 254]
+    fun <T> processData(input: T): T {
+        return input
+    }
 }
