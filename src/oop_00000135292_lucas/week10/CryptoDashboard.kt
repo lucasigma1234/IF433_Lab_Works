f74db01aac296950f98f23b3b739d3e4d780503c
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-05-20 12:12:19 +0700

    week10: (task) populate coin repository

--- src/oop_00000135292_lucas/week10/CryptoDashboard.kt
+++ src/oop_00000135292_lucas/week10/CryptoDashboard.kt
@@ -3,4 +3,7 @@ package oop_00000135292_lucas.week10
 fun main() {
     println("=== CRYPTO WALLET DASHBOARD ===")
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 1.25))
+    coinRepo.add(Coin("ETH", 14.8))
+    coinRepo.add(Coin("USDT", 500.0))
 }
