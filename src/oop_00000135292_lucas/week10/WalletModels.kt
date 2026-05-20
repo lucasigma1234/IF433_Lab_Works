1187ac36dd4f3bd983f9f594a169997c7e52e142
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-05-20 12:09:04 +0700

    week10: (task) implement search logic in repository

--- src/oop_00000135292_lucas/week10/WalletModels.kt
+++ src/oop_00000135292_lucas/week10/WalletModels.kt
@@ -10,4 +10,13 @@ class WalletRepository<T : Any> {
     fun getAll(): List<T> {
         return items
     }
+    fun searchByName(query: String): List<T> {
+        return items.filter { item ->
+            if (item is Coin) {
+                item.name.contains(query, ignoreCase = true)
+            } else {
+                item.toString().contains(query, ignoreCase = true)
+            }
+        }
+    }
 }
