84009e82f6b73641aab11092f18486616a7210a5
Lucas Stevanus Noel Siahaan <lucasiahaan1234@gmail.com>
2026-05-20 12:04:00 +0700

    week10: (task) implement generic WalletRepository

--- src/oop_00000135292_lucas/week10/WalletModels.kt
+++ src/oop_00000135292_lucas/week10/WalletModels.kt
@@ -2,3 +2,21 @@ package oop_00000135292_lucas.week10
 
 data class Coin(val name: String, val balance: Double)
 data class Transaction(val id: String, val amount: Double)
+class WalletRepository<T : Any> {
+    private val items = mutableListOf<T>()
+    fun add(item: T) {
+        items.add(item)
+    }
+    fun getAll(): List<T> {
+        return items
+    }
+    fun searchByName(query: String): List<T> {
+        return items.filter { item ->
+            if (item is Coin) {
+                item.name.contains(query, ignoreCase = true)
+            } else {
+                item.toString().contains(query, ignoreCase = true)
+            }
+        }
+    }
+}
